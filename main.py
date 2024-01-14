2944848548d4cc073aa9d36690ddcbb604ee9c70
justin <justin.rvd@gmail.com>
2024-01-14 03:11:02 -0600

    day22: use pandas and add columns to the app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
+
 with col1:
-    st.image("images/justin.jpg", width=300)
+    st.image("images/justin.jpg")
 
 with col2:
     st.title("Justin Bigishiro")
@@ -16,3 +18,24 @@ with col2:
     I am enjoying with different skills I have acquired since.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        # st.image(row["image"])
+        st.write(row["description"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        # st.image(row["image"])
+        st.write(row["description"])
