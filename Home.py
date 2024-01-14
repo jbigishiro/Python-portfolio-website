61bb337c0e5eae4c743ca26ce8a35b2ffe8a8812
justin <justin.rvd@gmail.com>
2024-01-14 05:55:28 -0600

    add another page contact_us.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ with col2:
     st.title("Justin Bigishiro")
     content = """"
     Hey, I am Justin! I am a software Engineer. 
-    I graduated in 2011 with a bachelor's degree in Electronics Engineer. 
+    I graduated in 2011 with a bachelor's degree in Electronics Engineering. 
     I have been a high school teacher form 2012 to 2016 in Rwanda.
     In 2023, I decided to change the career to Software Engineering. 
     I am enjoying with different skills I have acquired since.
