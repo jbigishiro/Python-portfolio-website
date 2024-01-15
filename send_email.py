82d6a29d152e86375ddc78c2d30f2809d5737462
justin <justin.rvd@gmail.com>
2024-01-15 06:50:05 -0600

    connect send_email to contact form

--- send_email.py
+++ send_email.py
@@ -1,21 +1,16 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "justin.bigishiro@gmail.com"
-password = "bdawhctskdlajzfk"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "justin.bigishiro@gmail.com"
+    password = "bdawhctskdlajzfk"
+    receiver = "justin.rvd@gmail.com"
+    my_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-receiver = "justin.rvd@gmail.com"
-my_context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
